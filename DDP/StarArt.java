d9e8fa4733629bc68c8021c2a6fb57e8e3b794a7
aydrava <ayudiahravlini@gmail.com>
2022-09-22 15:31:04 +0700

    menambahkan index.html,about.html,contact.html

---
+++ DDP/StarArt.java
@@ -0,0 +1,12 @@
+public class StarArt {
+	public static void main(String[] args) {
+		
+		System.out.println("StarArt");
+		System.out.println("Bit/1402020000");
+		System.out.println("---------------------------------------");
+		System.out.println("Halo, nama saya Bit, mahasiswa Teknik Informatika, Universitas YARSI, angkatan " + args[0] + ", yang berasal dari " + args[1] + ". Saya tertarik kuliah di jurusan ini karena ingin membuat " + args[2] + " " + args[3] + ".");
+
+		System.out.println("Salam,");
+
+	}
+}
